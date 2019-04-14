e0ec2bf26bfb75022abd98c83817a7bcb536c64e
Nam Phan <namp323@gmail.com>
2019-04-14 17:45:10 +1000

    add responsiveness to cloud chat iframe

--- src/CloudChatCard/CloudChatCard.jsx
+++ src/CloudChatCard/CloudChatCard.jsx
@@ -1,16 +1,50 @@
-import React, { PureComponent } from 'react';
+import React, { Component, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import SynarenCard from '../Shared/SynarenCard';
 import { Iframe } from './CloudChatCard.styles';
 
-class CloudChatCard extends PureComponent {
+function screenWidth() {
+  const w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth;
+  return x;
+}
+
+class CloudChatCard extends Component {
+  state = {
+    screenWidth: screenWidth(),
+    throttle: undefined
+  };
+
+  updateScreenWidth = () => {
+    const { throttle } = this.state;
+    if(throttle){
+      clearTimeout(throttle);
+    }
+    const newThrottle = setTimeout(_ => this.setState({screenWidth: screenWidth()}), 500);
+    this.setState({ throttle: newThrottle });
+  }
+
+  componentDidMount(){
+    this.updateScreenWidth();
+    window.addEventListener("resize", this.updateScreenWidth);
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener("resize", this.updateScreenWidth);
+  }
+
   render() {
+    const { screenWidth } = this.state;
+    const screenContentWidth = screenWidth > 568 ? "680px" : "800px";
     return (
       <SynarenCard
         isImageCardReverse={false}
         isImageCard={true}
         descriptionStyle={{ padding: "50px", zIndex:"2" }}
-        contentStyle={{height: "680px", marginBottom:"0", marginTop:"-48px"}}
+        contentStyle={{height: screenContentWidth, marginBottom:"0", marginTop:"-48px"}}
         content
         content={
           <Iframe
