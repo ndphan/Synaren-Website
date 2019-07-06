265251fb9c6738e75fb36b1f2a9e9fff38fb2361
Nam Phan <namp323@gmail.com>
2019-07-06 21:24:48 +1000

    add npm and github icons to intro

--- src/IntroCard/IntroCard.jsx
+++ src/IntroCard/IntroCard.jsx
@@ -17,6 +17,12 @@ class IntroCard extends PureComponent {
             Connect with me on <a href="https://www.linkedin.com/in/ndphan/">Linkedin</a>.
             <br/>
             Find anything you like?
+            <div className="uk-margin-small-top">
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/ndphan/" uk-icon="icon: github;" className="uk-link">&#8203;</a>&nbsp;&nbsp;
+              <a target="_blank" rel="noopener noreferrer" href="https://www.npmjs.com/~ndphan" style={{height:"32px",width:"32px"}} className="uk-icon npm-icon uk-link">
+                <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="npm" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M288 288h-32v-64h32v64zm288-128v192H288v32H160v-32H0V160h576zm-416 32H32v128h64v-96h32v96h32V192zm160 0H192v160h64v-32h64V192zm224 0H352v128h64v-96h32v96h32v-96h32v96h32V192z"/></svg>
+              </a>
+            </div>
           </div>
         }
         content={
