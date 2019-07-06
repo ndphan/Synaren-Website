997315cbea0203afa49c340bfb2e8eca62be0702
Nam Phan <namp323@gmail.com>
2019-07-06 21:23:15 +1000

    remove app insights

--- src/index.js
+++ src/index.js
@@ -4,16 +4,10 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import "uikit";
-import ReactAI from "react-appinsights";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
-ReactAI.init(
-  { instrumentationKey: "4962a06e-429d-4207-9232-2f216a3819ee" },
-  history
-);
-ReactAI.ai().trackPageView("Synaren", window.location);
 
 ReactDOM.render(
   <Router history={history}>
