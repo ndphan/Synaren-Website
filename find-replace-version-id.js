8771d138d5723b49b67598d81a1591727e2b7d03
Nam Phan <namp323@gmail.com>
2019-12-28 07:36:41 +1100

    update find replace version to have build date

--- find-replace-version-id.js
+++ find-replace-version-id.js
@@ -3,17 +3,19 @@ const fs = require('fs');
 const productionFile = "./.env.production";
 
 function randomBuildId() {
-	return 'xxxx-xxxx'.replace(/[xy]/g, char => {
-			var r = Math.random() * 16 | 0, v = char == 'x' ? r : (r & 0x3 | 0x8);
-			return v.toString(16);
+	const date = new Date().toISOString().split('T')[0];
+	const randomHash = 'xxxx'.replace(/[xy]/g, char => {
+		const r = Math.random() * 16 | 0, v = char == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
 	});
+	return `${date}-${randomHash}`
 }
 
 fs.readFile(productionFile, 'utf8', (err, data) => {
-  if (err) return console.log(err);
+		if (err) return console.log(err);
 		fs.writeFile(
 			productionFile, 
-			data.replace(/\REACT_APP_API_VERSION_NUMBER=[\d\w\-]+/g, `REACT_APP_API_VERSION_NUMBER=${randomBuildId()}`), 
+			data.replace(/\REACT_APP_API_VERSION_NUMBER=[\d\w\-]*/g, `REACT_APP_API_VERSION_NUMBER=${randomBuildId()}`), 
 			'utf8', 
 			err => err && console.log(err)
 		);
